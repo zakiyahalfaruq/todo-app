90e8900db483eac74f12d573fac3ff1bdce39d11
Zakiyah Al-Faruq <alfaruqzakiyah@gmail.com>
2025-07-03 11:23:57 +0100

    Refactor app.py to support task list rendering

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+tasks = []
+
 @app.route('/')
-def home():
-    return 'Hello, To-Do App!'
-    
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form.get('task')
+    if task:
+        tasks.append(task)
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
